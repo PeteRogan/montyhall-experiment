f54945ec8db5510935daedaa57419d8d36ec3a59
PeteRogan <petr_rogan@live.ca>
2025-07-20 15:07:56 -0500

    Update MontyHallExperiment.py
    Changed "boxes" to "doors"

--- MontyHallExperiment.py
+++ MontyHallExperiment.py
@@ -1,5 +1,5 @@
-# 1 - car; 0 - goat
-# Scenario 1 - keep, scenario 2 - switch
+# doors: 1 - car; 0 - goat
+# scenario: 1 - keep, 2 - switch
 import random
 
 n_runs = int(input("Enter number or runs: "))
@@ -7,26 +7,26 @@ scenario = int(input("Enter scenario (1 or 2): "))
 n_wins = 0
 
 for i in range(n_runs):
-    boxes = [1, 0, 0]
+    doors = [1, 0, 0]
     
     #Get first choice
     index1 = random.randint(0, 2)
-    choice1 = boxes[index1]
+    choice1 = doors[index1]
     
     #Remove first choice
-    del boxes[index1]
+    del doors[index1]
     
     #Remove one non-car
-    if boxes[0] == 0:
-        del boxes[0]
+    if doors[0] == 0:
+        del doors[0]
     else:
-        del boxes[1]
+        del doors[1]
     
     #Implement scenario
     if scenario == 1:
         choice2 = choice1
     elif scenario == 2:
-        choice2 = boxes[0]
+        choice2 = doors[0]
     
     #Determine if won
     if choice2 == 1:
